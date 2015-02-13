361de6a6e25fe5fdf2580daf6067239057669d1b
Bachors <bachor.can@gmail.com>
2015-02-13 23:26:05 +0700

    g+ & stumbleupon

--- gp_stup.php
+++ gp_stup.php
@@ -18,21 +18,6 @@
 			$json = json_decode($curl_results, true);
 			return isset($json[0]['result']['metadata']['globalCounts']['count'])?intval( $json[0]['result']['metadata']['globalCounts']['count'] ):0;
 		}
-		private function file_get_contents_curl($url){
-			$ch=curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-			curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-			curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
-			$cont = curl_exec($ch);
-			if(curl_error($ch))
-			{
-				die(curl_error($ch));
-			}
-			return $cont;
-		}
 	}
 	
 	function share($url){
