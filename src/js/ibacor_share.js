5e8c81dc3711ad18c631152212d7833ad4682d74
coscho <coscho06@yahoo.com>
2015-12-18 20:05:32 +0200

    unminify file

---
+++ src/js/ibacor_share.js
@@ -0,0 +1,140 @@
+/*********************************************************************
+ * #### jQuery Awesome Sosmed Share Button v03 ####
+ * Coded by Ican Bachors 2014.
+ * http://ibacor.com/labs/jquery-awesome-sosmed-share-button/
+ * Updates will be posted to this site.
+ *********************************************************************/
+// install
+$(document).ready(function() {
+        eval(function(p, a, c, k, e, r) {
+            e = function(c) {
+                return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+            };
+            if (!''.replace(/^/, String)) {
+                while (c--) r[e(c)] = k[c] || e(c);
+                k = [function(e) {
+                    return r[e]
+                }];
+                e = function() {
+                    return '\\w+'
+                };
+                c = 1
+            };
+            while (c--)
+                if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+            return p
+        }('1p b=1q(9.c),a=\'\',d=\'\',K=\'\',8=\'\';L($(M).j(\'6\')!=N){a+=$(M).j(\'6\')}L($(\'O[13="P"]\').j(\'Q\')!=N){d+=$(\'O[13="P"]\').j(\'Q\')}1r L($(M).j(\'6\')!=N){d+=$(M).j(\'6\')}L($(\'O[14="15:16"]\').j(\'Q\')!=N){K+=$(\'O[14="15:16"]\').j(\'Q\')}8+=\'<p><a c="k://I.R.4/S/S.17?u=\'+b+\'" l="m:n o.q(\\\'k://I.R.4/S/S.17?u=\'+b+\'\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-R" 6="1s"><i 3="2 2-R"></i> <5 f="1t"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><a c="H://T.4/J?U=\'+a+\'+-+V @W&7=\'+b+\'" l="m:n o.q(\\\'H://T.4/J?U=\'+a+\'+-+V @W&7=\'+b+\'\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-T" 6="1u"><i 3="2 2-T"></i> <5 f="1v"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><a c="H://12.X.4/J?7=\'+b+\'" l="m:n o.q(\\\'H://12.X.4/J?7=\'+b+\'\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-X" 6="1w+"><i 3="2 2-X-12"></i> <5 f="1x"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><a c="k://Y.4/18?7=\'+b+\'&6=\'+a+\'+-+V @W" l="m:n o.q(\\\'k://Y.4/18?7=\'+b+\'&6=\'+a+\'+-+V @W\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-Y" 6="1y"><i 3="2 2-Y"></i> <5 f="1z"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><a c="H://I.Z.4/19?1a=1b&7=\'+b+\'&6=\'+a+\'&1c=\'+d+\'" l="m:n o.q(\\\'H://I.Z.4/19?1a=1b&7=\'+b+\'&6=\'+a+\'&1c=\'+d+\'\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-Z" 6="1A"><i 3="2 2-Z"></i> <5 f="1B"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><a c="k://10.4/1d/1e/1f/?7=\'+b+\'&1g=\'+K+\'&P=\'+d+\'" l="m:n o.q(\\\'k://10.4/1d/1e/1f/?7=\'+b+\'&1g=\'+K+\'&P=\'+d+\'\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-10" 6="1C"><i 3="2 2-10"></i> <5 f="1D"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><a c="k://I.11.4/1h/?7=\'+b+\'" l="m:n o.q(\\\'k://I.11.4/1h/?7=\'+b+\'\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-11" 6="1E"><i 3="2 2-11"></i> <5 f="1F"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><a c="H://1i.4/1j?7=\'+b+\'&U=\'+d+\'" l="m:n o.q(\\\'H://1i.4/1j?7=\'+b+\'&U=\'+d+\'\\\',\\\'r.4\\\',\\\'s=t,v=w,x=0,y=0,9=0,z=1,A=1,B=1,C=0,D=0\\\');E F;" 3="G e-1G" 6="1H"><i 3="2 2-1I"></i> <5 f="1J"><i 3="2 2-g 2-h"></i></5></a></p>\';8+=\'<p><5 3="e-1K" 6="\'+1L(\'%1M%1k%1l%1m%1N%1n%1o%1O%1n%1P%1o%1l%1Q%1k%1R%1m%1S\')+\'"><i 3="2 2-J-1T"></i> <5 f="1U"><i 3="2 2-g 2-h"></i></5></5></p>\';$(\'#1V\').8(8);1W(b)', 62, 121, '||fa|class|com|span|title|url|html|location|||href|desk|sh|id|spinner|spin||attr|http|onclick|javascript|void|window||open|ibacor|width|700||height|500|toolbar|menubar|status|scrollbars|resizable|left|top|return|false|sh_btn|https|www|share|img|if|document|null|meta|description|content|facebook|sharer|twitter|text|via|bachors|google|reddit|linkedin|pinterest|stumbleupon|plus|name|property|og|image|php|submit|shareArticle|mini|true|summary|pin|create|button|media|badge|bufferapp|add|68|61|72|20|62|var|encodeURIComponent|else|Facebook|ibacor_count_fb|Twitter|ibacor_count_tw|Google|ibacor_count_gp|Reddit|ibacor_count_rd|Linkedin|ibacor_count_in|Pinterest|ibacor_count_pn|Stumbleupon|ibacor_count_up|buffer|Bufferapp|bars|ibacor_buffer|total|unescape|53|65|79|40|63|6F|73|alt|ibacor_count_sh|ibacor_shareawesome|ibacor_google_stumbleupon'.split('|'), 0, {}))
+    })
+    // function
+function ibacor_google_stumbleupon(d) {
+    $.ajax({
+        url: 'https://count.donreach.com/?url=' + d,
+        crossDomain: true,
+        dataType: 'jsonp'
+    }).done(function(a) {
+        var b = a.shares.google,
+            htmlupon = '';
+        if (a.shares.stumbleupon != null) {
+            htmlupon += a.shares.stumbleupon
+        } else {
+            htmlupon += 0
+        }
+        $('#ibacor_count_gp').html(b);
+        $('#ibacor_count_up').html(htmlupon);
+        var c = parseInt(b) + parseInt(htmlupon);
+        ibacor_buffer(d, c)
+    }).fail(function() {
+        $('#ibacor_count_gp').html('0');
+        $('#ibacor_count_up').html('0');
+        ibacor_buffer(d, 0)
+    })
+}
+
+function ibacor_buffer(d, e) {
+    $.ajax({
+        url: 'https://api.bufferapp.com/1/links/shares.json?url=' + d,
+        crossDomain: true,
+        dataType: 'jsonp'
+    }).done(function(a) {
+        var b = a.shares;
+        $('#ibacor_buffer').html(b);
+        var c = parseInt(b) + parseInt(e);
+        ibacor_facebook(d, c)
+    })
+}
+
+function ibacor_facebook(d, e) {
+    $.ajax({
+        url: 'https://api.facebook.com/method/links.getStats?urls=' + d + '&format=json',
+        crossDomain: true,
+        dataType: 'jsonp'
+    }).done(function(a) {
+        var b = a[0].total_count;
+        $('#ibacor_count_fb').html(b);
+        var c = parseInt(b) + parseInt(e);
+        ibacor_twitter(d, c)
+    })
+}
+
+function ibacor_twitter(d, e) {
+    $.ajax({
+        url: 'http://cdn.api.twitter.com/1/urls/count.json?url=' + d,
+        crossDomain: true,
+        dataType: 'jsonp'
+    }).done(function(a) {
+        var b = a.count;
+        $('#ibacor_count_tw').html(b);
+        var c = parseInt(b) + parseInt(e);
+        ibacor_linkedin(d, c)
+    }).fail(function() {
+        $('#ibacor_count_tw').html('0');
+        ibacor_linkedin(d, e)
+    })
+}
+
+function ibacor_linkedin(d, e) {
+    $.ajax({
+        url: 'https://www.linkedin.com/countserv/count/share?url=' + d + '&callback=?',
+        crossDomain: true,
+        dataType: 'json'
+    }).done(function(a) {
+        var b = a.count;
+        $('#ibacor_count_in').html(b);
+        var c = parseInt(b) + parseInt(e);
+        ibacor_pinterest(d, c)
+    })
+}
+
+function ibacor_pinterest(d, e) {
+    $.ajax({
+        url: 'http://api.pinterest.com/v1/urls/count.json?url=' + d + '&callback=?',
+        crossDomain: true,
+        dataType: 'json'
+    }).done(function(a) {
+        var b = a.count;
+        $('#ibacor_count_pn').html(b);
+        var c = parseInt(b) + parseInt(e);
+        ibacor_reddit(d, c)
+    })
+}
+
+function ibacor_reddit(d, e) {
+    $.ajax({
+        url: 'http://www.reddit.com/api/info.json?url=' + d,
+        crossDomain: true,
+        dataType: 'json'
+    }).done(function(a) {
+        var b;
+        if (a.data.children != '') {
+            b = a.data.children[0].data.score
+        } else {
+            b = 0
+        }
+        $('#ibacor_count_rd').html(b);
+        var c = parseInt(b) + parseInt(e);
+        $('#ibacor_count_sh').html(c)
+    }).fail(function() {
+        $('#ibacor_count_rd').html('0');
+        $('#ibacor_count_sh').html(e)
+    })
+}
