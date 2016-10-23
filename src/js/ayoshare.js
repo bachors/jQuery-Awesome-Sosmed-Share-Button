f29401f6ecd95600f780cc8d865ddbb6cb360510
riochr17 <riochr17@gmail.com>
2016-10-23 11:31:18 +0700

    perbaikan url Line It share

--- src/js/ayoshare.js
+++ src/js/ayoshare.js
@@ -79,9 +79,7 @@ $.fn.ayoshare = function(h, j, k, l, m, n, o, p, q, r, email, whatsapp, telegram
 			html += '<i class="mobile"><i class="fa fa-envelope"></i></i></a></div>';
         }
         if (line == true) {
-            // http://lineit.line.me automatically detected and directly open Line Messenger application
-            // It should not use Line protocol, e.g. line://...
-            html += '<div class="line button"><a href="https://lineit.line.me/share/ui?url=' + b + '" target="_BLANK" title="Line">';
+            html += '<div class="line button"><a href="line://msg/text/' + b + '" target="_BLANK" title="Line">';
             html += '<i class="mobile"><i class="fa fa-line"></i></i></a></div>';
         }
         $(this).html('<div class="ayoshare">' + html + '</div>');
